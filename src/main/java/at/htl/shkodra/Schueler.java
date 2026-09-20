b172050b300a646f32d426e2b8a0415ce10003a1
Regis Balla <ballaregis2008@gmail.com>
2026-09-20 08:49:15 +0200

    Übungsblatt Maven: Übung 1-5 erledigt

---
+++ src/main/java/at/htl/shkodra/Schueler.java
@@ -0,0 +1,19 @@
+package at.htl.shkodra;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.util.List;
+
+public record Schueler(String name, int jahrgang, String gruppe) {
+
+    public static void main(String[] args) {
+        List<Schueler> liste = List.of(
+                new Schueler("Ana Hoxha", 5, "5as"),
+                new Schueler("Blerta Krasniqi", 5, "5aw"),
+                new Schueler("Dritan Berisha", 5, "5an")
+        );
+
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        System.out.println(gson.toJson(liste));
+    }
+}
